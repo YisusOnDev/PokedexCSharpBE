f8df89423027fd01117c7088d462af0874248531
Yisus <jesustellez.contacto@gmail.com>
2021-05-15 12:22:57 +0200

    refactor("Now pokemon types are just Strings").

--- PokedexWebBE.DAL/Implementations/PokemonRepository.cs
+++ PokedexWebBE.DAL/Implementations/PokemonRepository.cs
@@ -30,40 +30,14 @@ namespace PokedexWebBE.DAL.Implementations
                 {
                     while (reader.Read())
                     {
-                        Pokemon pokeToAdd = new Pokemon(reader.GetInt32("Numero"),reader.GetString("Nombre"), reader.GetString("Descripcion"), reader.GetFloat("Altura"), reader.GetFloat("Peso"), reader.GetString("Habilidad"), reader.GetString("Categoria"), reader.GetString("ImagenURL"), reader.GetString("SonidoURL"), getPokemonTypes(reader.GetString("Nombre")));
+                        Pokemon pokeToAdd = new Pokemon(reader.GetInt32("Numero"), reader.GetString("Nombre"), reader.GetString("Descripcion"), reader.GetFloat("Altura"), reader.GetFloat("Peso"), reader.GetString("Habilidad"), reader.GetString("Categoria"), reader.GetString("ImagenURL"), reader.GetString("SonidoURL"), reader.GetString("Tipos"));
                         pokeList.Add(pokeToAdd);
-                       
+
                     }
-                    return new GenericAPIResponse<List<Pokemon>>(pokeList); 
+                    return new GenericAPIResponse<List<Pokemon>>(pokeList);
                 }
             }
 
-            return null;
-        }
-
-        public String getPokemonTypes(String pokeName)
-        {
-            using (MySqlConnection conn = context.GetConnection())
-            {
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand("SELECT p.numero, t.tipo from tipos t INNER JOIN pokemon_tipos pt ON pt.codigotipo = t.codTipo INNER JOIN pokemon p ON p.numero = pt.numero WHERE p.Nombre = '" + pokeName + "'", conn);
-                List<String> pokeTypes = new List<String>();
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        pokeTypes.Add(reader.GetString("tipo"));
-                    }
-
-                    String toReturnTypes = "";
-                    foreach (String type in pokeTypes)
-                    {
-                      toReturnTypes += " " + type + " ";
-                    }
-
-                    return toReturnTypes;
-                }
-            }
             return null;
         }
 
